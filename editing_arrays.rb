722e6cf66f715f524f6fdb459df3e67fe7b54fef
agalbokke <ashyahgalbokke@gmail.com>
2018-07-11 18:25:34 +0000

    automatically backed up by learn

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -6,13 +6,13 @@ RAINBOW_COLORS = [
 
 def change_rainbow_colors
   RAINBOW_COLORS[0] = "red"
-  RAINBOW_COLORS[1] = "orange"
+  RAINBOW_COLORS[1] = "light_red"
   RAINBOW_COLORS[2] = "light_yellow"
   return RAINBOW_COLORS 
 end
 
 
 def add_colors
-  RAINBOW_COLORS << "green"
+  RAINBOW_COLORS.push "green"
   RAINBOW_COLORS << "blue"
 end
